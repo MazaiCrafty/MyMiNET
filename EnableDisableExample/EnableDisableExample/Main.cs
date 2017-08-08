977e5f4f013b55ae7d711ac603249a0e1a59203d
Neko74 <takatim1574@icloud.com>
2017-08-08 16:18:17 +0900

    修正

---
+++ EnableDisableExample/EnableDisableExample/Main.cs
@@ -0,0 +1,39 @@
+﻿using MiNET.Plugins;
+using log4net;
+using MiNET.Plugins.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamplePlugin
+{
+    [Plugin(PluginName = "EnableDisableExample", Description = "Example Plugin", PluginVersion = "1.2", Author = "_ICE_")]
+    public class Main : Plugin
+    {
+        protected override void OnEnable()
+        {
+            base.OnEnable(); 
+            Console.Write("[ExamplePlugin] Enable!!");
+        }
+
+        [Command(Command = "hello", Permission = "User")]
+        public void hello(Player sender)
+        {
+            sender.SendMessage("Welcome!!", MessageType.Raw);
+        }
+
+        [Command(Command = "good", Permission = "OP")]
+        public void good(Player sender)
+        {
+            sender.SendMessage("Good!!", MessageType.Raw);
+        }
+
+        protected override void OnDisabe()
+        {
+            base.OnDisabe();
+            Console.Write("[ExamplePlugin] Disable!!");
+        }
+    }
+}
